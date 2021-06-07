b52e2d9411ce87195a4498eed42c73de00e9f9b3
halitakkus <halitakkus03@gmail.com>
2021-06-07 11:33:10 +0300

    Web UI eklendi.

---
+++ src/Asmin.WebMVC/Services/Rest/Base/IHttpService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace AES.WebMVC.Services.Rest.Base
+{
+    public interface IHttpService
+    {
+        Task<T> GetAsync<T>(string url);
+        Task<T> PostAsync<T>(string url, object data);
+    }
+}
---
+++ src/Asmin.WebMVC/Services/Session/ISessionService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asmin.WebMVC.Services.Session
+{
+    /// <summary>
+    /// Register your session data and use it.
+    /// </summary>
+    public interface ISessionService
+    {
+        /// <summary>
+        /// Set data to session.
+        /// </summary>
+        /// <param name="key">Session unique key.</param>
+        /// <param name="obj">Session data.</param>
+        void Set(string key, object obj);
+
+        /// <summary>
+        /// Get data. Returns default when key not found.
+        /// </summary>
+        /// <typeparam name="T">Data type.</typeparam>
+        /// <param name="key">Unique key.</param>
+        /// <returns></returns>
+        T Get<T>(string key) where T : class;
+
+        /// <summary>
+        /// Remove data from session when key exists.
+        /// </summary>
+        /// <param name="key"></param>
+        void Remove(string key);
+
+        /// <summary>
+        /// Check specified key exists in session.
+        /// </summary>
+        /// <param name="key">Unique key.</param>
+        /// <returns></returns>
+        bool Any(string key);
+
+        /// <summary>
+        /// Clear all data from session.
+        /// </summary>
+        void Clear();
+    }
+}
